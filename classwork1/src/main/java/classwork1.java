c7cc933230ad1325c52f03f65ad49ffd38cb8fd4
chehlov.evgeniy <chehlov.evgeniy@gmail.com>
2019-09-23 21:00:37 +0300

    classroom

---
+++ classwork1/src/main/java/classwork1.java
@@ -0,0 +1,73 @@
+public class classwork1 {
+
+    public static void main(String[] args) {
+//Блок переменных задание 1
+        float V1 = 5f;
+        float V2 = 10f;
+        float V;
+        float t1 = 20f;
+        float t2 = 30f;
+        float t;
+
+// Блок переменных задание 2
+        float grn=1000f;
+        float fs=30.10f;
+        float doll=24.80f;
+        float evro=30.60f;
+
+//Блок переменных задание 3
+        int a1;
+        int a2;
+        int a4;
+        int a6;
+
+// Блок переменных задание 4ж
+
+int a;
+int b;
+int c;
+
+// Задание 1
+        V = V1+V2;
+        t=(V1*t1+t2*V2)/V;
+        System.out.println("Итогавая температура равна =" + t);
+// Задание 2
+// Вычисляем сколько будет каждой валюты на 1000 грнж
+        doll=1000/doll;
+        evro=1000/evro;
+        fs=1000/fs;
+        System.out.println("Количество евро" + );
+        System.out.println("Количество долларов");
+        System.out.println("Количество фунтов");
+
+// Задание 3
+        a2=a1*a1;
+        a4=a2*a2;
+
+        a2=a1*a1;
+        a4=a2*a2;
+        a6=a2*a4;
+        System.out.println("a4 = " + a4);
+        System.out.println("a6 = " + a6);
+
+// Задание 4 a
+        System.out.println("My String");
+        a=5;
+        b=10;
+        a=c;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+
+        // б
+        a=5;
+        b=10;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
