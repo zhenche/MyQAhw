8388365b4b64878dccb12fdbeca1558f0913f2b7
chehlov.evgeniy <chehlov.evgeniy@gmail.com>
2019-10-08 15:33:53 +0300

    рц4_3_3_10

--- homework4/3_3_10/src/main/java/Hw4_3_3_10.java
+++ homework4/3_3_10/src/main/java/Hw4_3_3_10.java
@@ -1,6 +1,19 @@
 public class Hw4_3_3_10 {
-
+/*
+"10*. Write a program which prints the next figure to the console:
+**********
+*           *
+*           *
+*           *
+**********"
+ */
     public static void main(String[] args) {
+        System.out.println("**********");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("*           *");
+        System.out.println("**********\"");
+// Поскольку в задании жёстко не указано каким образом необходимо это реализовать, то делаю по наипростешему пути :)
 
     }
 
