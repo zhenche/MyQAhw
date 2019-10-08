53b6690878af67d3424c9043711a85fcbfb348f5
chehlov.evgeniy <chehlov.evgeniy@gmail.com>
2019-10-08 12:49:32 +0300

    hw4_3_3_6 and 3_3_7 mod

--- homework4/3_3_6/src/main/java/hw4_3_3_6.java
+++ homework4/3_3_6/src/main/java/hw4_3_3_6.java
@@ -2,8 +2,7 @@ public class hw4_3_3_6 {
 /*
 6. Write a method named getEvenDigitSum with one parameter
 of the type "int". The method should return the sum of even
-digits within the number.
-If the number is negative, method should return -1;
+digits within the number. If the number is negative, method should return -1;
  */
     public static void main(String[] args) {
     int number=3467;
@@ -18,23 +17,15 @@ If the number is negative, method should return -1;
         a4 = a - (a1 * 1000) - (a2 * 100) - (a3 * 10);
         int[] Array = {3, 4, 6, 7};
         System.out.println(a);
-        System.out.println(a1);
-        System.out.println(a2);
-        System.out.println(a3);
-        System.out.println(a4);
-        System.out.println(Array);
+        System.out.println("a1="+ a1 + " a2=" + a2 + " a3=" + a3 + " a4=" + a4);
         for (i = 0; i < 4; i++) {
             even = Array[i];
-
             if (even % 2 == 0) {
                 summ = summ + Array[i];
-                System.out.println(Array[i]);
             } else {
                 if (summ < 0) return -1;
             }
-
         }
         return summ;
     }
-
 }
--- homework4/3_3_7/src/main/java/hw4_3_3_7.java
+++ homework4/3_3_7/src/main/java/hw4_3_3_7.java
@@ -1,30 +1,25 @@
 public class hw4_3_3_7 {
-
 /*
 "7. Write a method named getGreatestCommonDivisor with two parameters of type int named first and second.
 If one of the parameters is < 10, method should return -1 to indicate an invalid value.
 The method should return the greatest common divisor of two numbers."
  */
-
     public static void main(String[] args) {
     int first=25,second=75;
         System.out.println(getGreatestCommonDivisor(first,second));
         first=4;
         System.out.println(getGreatestCommonDivisor(first,second));
     }
-
     public static int getGreatestCommonDivisor(int a, int b) {
     int m=0;
     if (a<10 | b<10) {
         m= -1;
-        }
-    else {
+        } else {
         for (int i=10;i<=100;i++) {
         if ((a % i == 0) && (b % i == 0)) {
             m = i;
             return m;
         } else {
-
             m = 0;
         }
     }
