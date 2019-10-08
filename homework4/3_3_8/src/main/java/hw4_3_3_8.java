f0ddcc5cfc46d38e673b67121b666118a08f82c9
chehlov.evgeniy <chehlov.evgeniy@gmail.com>
2019-10-08 12:25:17 +0300

    hw4_3_3_8

---
+++ homework4/3_3_8/src/main/java/hw4_3_3_8.java
@@ -0,0 +1,17 @@
+public class hw4_3_3_8 {
+/*
+"8. Write a program which prints the next figure to the console:
+**********
+**********
+**********
+**********"
+ */
+    public static void main(String[] args) {
+        for (int i=1;i<=4;i++) {
+            for (int j=1;j<=10;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
