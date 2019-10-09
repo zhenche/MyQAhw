18cfe97578e6b70687a4a084a212329786dd342d
chehlov.evgeniy <chehlov.evgeniy@gmail.com>
2019-10-09 18:08:15 +0300

    Hw_3_2_1_this

--- homework5/3_2_1/src/main/java/Human.java
+++ homework5/3_2_1/src/main/java/Human.java
@@ -7,13 +7,18 @@ weight, age, first name, last name, height."
     String fname, lname;
 
 
+    public Human(String fname, String lname, int weight, int age, int height) {
+
+        this.fname = fname;
+        this.lname = lname;
+        this.weight = weight;
+        this.age = age;
+        this.height = height;
+
+    }
+
     public static void main(String[] args) {
-        Human man = new Human();
-        man.weight = 70;
-        man.age = 35;
-        man.height=170;
-        man.fname="Vasya";
-        man.lname="Pupkin";
+        Human man = new Human("Вася", "Пупкин", 70, 35, 170);
         System.out.println("ФИО:  " + man.lname + " " + man.fname);
         System.out.println("Основные данные: ");
         System.out.println("Вес: " + man.weight + " Рост: " + man.height + " Возраст: " + man.age);
