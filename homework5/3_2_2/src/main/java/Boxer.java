a7c710a5eb3ab6b84d5740ea308d9e2b0b1c27f4
chehlov.evgeniy <chehlov.evgeniy@gmail.com>
2019-10-09 17:28:21 +0300

    Hw_3_2_1

---
+++ homework5/3_2_2/src/main/java/Boxer.java
@@ -0,0 +1,26 @@
+public class Boxer {
+/*
+"2. Implement the method fight() depending on weight, strength and age of the boxer:
+You should write dependency by yourself. The method should return true or false if our boxer won or lost.
+*/
+    public int age;
+    public int weight;
+    public int strength;
+
+    public Boxer(int age, int weight, int strength) {
+        this.age = age;
+        this.weight = weight;
+        this.strength = strength;
+    }
+
+    public boolean fight(Boxer anotherBoxer) {
+        // write your code here
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+}
