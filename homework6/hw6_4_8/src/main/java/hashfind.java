f2727f4fded90ae0408dc20e6a47e67c38d43b1a
chehlov.evgeniy <chehlov.evgeniy@gmail.com>
2019-11-20 02:44:23 +0200

    hw6_4_8

--- homework6/hw6_4_8/src/main/java/hashfind.java
+++ homework6/hw6_4_8/src/main/java/hashfind.java
@@ -1,9 +1,10 @@
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class hashfind {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
             HashMap<String, String> myHashMap = new HashMap<>();
 
@@ -16,16 +17,31 @@ public class hashfind {
             myHashMap.put("iris", "flower");
             myHashMap.put(" potato", "vegetable");
             myHashMap.put("carrot", "vegetable");
+            String vegetals = "vegetable";
 
-            for(Map.Entry<String, String> entry: myHashMap.entrySet()) {
-                if (myHashMap.get("vegetable") == String.valueOf(true)) {
-                    System.out.printf(myHashMap.getValue().getName());}
-                else {
-                    System.out.println("nah");
-                }
+            processMap(myHashMap);
+
+        for (Map.Entry<String, String> entry : myHashMap.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (value == vegetals) {
+                System.out.println("Found: " + entry.getKey() + " = " + entry.getValue());
             }
-
         }
+    }
+
+    public static void processMap(Map mp) {
+        Iterator it = mp.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            if (pair.getValue() == "vegetable") {
+                System.out.println("Found: " + pair.getKey() + " = " + pair.getValue());
+            }
+            it.remove(); // дабы не было ConcurrentModificationException
+        }
+    }
+
+
 }
     
     
--- homework6/hw6_4_9/src/main/java/hw6_4_9.java
+++ homework6/hw6_4_9/src/main/java/hw6_4_9.java
@@ -11,21 +11,21 @@ public class Numer {
     public static void main(String[] args) {
 
         ArrayList<Integer> nums = new ArrayList<Integer>();
-        Numer num = new Numer("Томас");
+        Numer num = new Numer();
 
         nums.add(1);
 
         int lenArr = cats.size();
 
-        Cat[] catsArray = cats.toArray(new Cat[0]);
-        String str;
+//        Cat[] catsArray = cats.toArray(new Cat[0]);
+//        String str;
 
         int max = 0;
         for (int i=0; i<lenArr; i++) {
-            if (i+1>=lenArr) break;
-            int len1 = catsArray[i].toString().length();
-            max = Math.max(max,catsArray[i].toString().length());
-//            System.out.println(max);
+//            if (i+1>=lenArr) break;
+//            int len1 = catsArray[i].toString().length();
+//            max = Math.max(max,catsArray[i].toString().length());
+////            System.out.println(max);
         }
 
         for (int i=0; i<lenArr; i++) {
